c43aa4ae496326859f8852ff906b283b45e3303b
hardinah <hardinah@novity.io>
2022-12-05 11:37:40 +0300

    :sparkles: Feature : custom table show/hide

---
+++ public/assets/js/custom/hideShowColumn.js
@@ -0,0 +1,38 @@
+$('#edit').click(function () {
+    var headers = $('#dt th').map(function () {
+        var th = $(this);
+        return {
+            text: th.text(),
+            shown: th.css('display') != 'none'
+        };
+    });
+    var h = ['<div id="tableEditor"><button class="submit" id="btn-1">Done</button><table><thead><tr>'];
+    $.each(headers, function () {
+        h.push('<th><input type="checkbox"', (this.shown ? ' checked ' : ' '), '/> ', this.text, '</th>');
+    });
+    h.push('</tr></thead></table></div>');
+    $('#containertable').parent().append(h.join(''));
+
+    $('.submit').click(function () {
+        var showHeaders = $('#tableEditor input').map(function () {
+            return this.checked;
+        });
+        $.each(showHeaders, function (i, show) {
+            var cssIndex = i + 1;
+            var tags = $('#dt th:nth-child(' + cssIndex + '), #dt td:nth-child(' + cssIndex + ')');
+            if (show)
+                tags.show();
+            else
+                tags.hide();
+
+
+
+        });
+        console.log
+        $('#tableEditor').remove();
+        return false;
+    });
+
+    return false;
+});
+
