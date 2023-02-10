5be8e8e99f3d54b71901f7ab04821262c631e7ec
hardinah <hardinah@novity.io>
2023-02-10 11:16:06 +0300

    🔨 Rafactor : Work

---
+++ migrations/Version20230210070146.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230210070146 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stack_tech_language ADD maintener VARCHAR(75) DEFAULT NULL, ADD language_level VARCHAR(100) NOT NULL, ADD open_source TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stack_tech_language DROP maintener, DROP language_level, DROP open_source');
+    }
+}
